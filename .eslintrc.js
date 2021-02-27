369283dcd66eca1ba0e08130ccebf3c68be507e3
Leo Liu <karta1041337@gmail.com>
2021-02-28 03:32:54 +0800

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,49 @@
+module.exports = {
+    'env': {
+        'es2020': true,
+        'node': true,
+    },
+    'extends': [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended',
+    ],
+    'parser': '@typescript-eslint/parser',
+    'parserOptions': {
+        'ecmaVersion': 12,
+        'sourceType': 'module',
+    },
+    'plugins': [
+        '@typescript-eslint',
+    ],
+    'rules': {
+        'indent': [
+            'error',
+            4,
+            {
+                'SwitchCase': 1,
+            },
+        ],
+        'quotes': [
+            'error',
+            'single',
+        ],
+        'semi': [
+            'error',
+            'always',
+        ],
+        'comma-dangle': [
+            'error',
+            'always-multiline',
+        ],
+        'sort-imports': [
+            'error',
+            {
+                'ignoreCase': false,
+                'ignoreDeclarationSort': false,
+                'ignoreMemberSort': false,
+                'memberSyntaxSortOrder': ['none', 'all', 'multiple', 'single'],
+                'allowSeparatedGroups': true,
+            },
+        ],
+    },
+};
---
+++ README.md
@@ -0,0 +1,3 @@
+# AWS Secrets Manager
+
+This package has helpers for AWS Secrets Manager SDK which makes rotating secrets from Lambda easier.
